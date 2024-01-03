441c97eac8ffe18ba09b78ddccbeac60474def62
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-01-03 15:38:37 +0100

    Added Minheap from Primeagen

---
+++ Primeagen answers/Minheap.ts
@@ -0,0 +1,106 @@
+export default class MinHeap
+{
+    public length: number;
+    private data : number[];
+
+    
+    constructor()
+    {
+        this.data = [];
+        this.length = 0;
+    }
+
+    insert(value: number): void
+    {
+        this.data[this.length] = value;
+        this.heapifyUp(this.length);
+        this.length++;
+    }
+    delete(): number
+    {
+        if (this.length === 0)
+        {
+            return -1;
+        }
+        
+        const out = this.data[0];
+        this.length--;
+
+        if (this.length === 0)
+        {
+            this.data = [];
+            return (out);
+        }
+
+        this.data[0] = this.data[this.length];
+        this.heapifDown(0);
+        return (out);
+    }
+
+    private heapifDown(idx : number): void
+    {
+        const lIdx = this.leftChild(idx);
+        const rIdx = this.rightChild(idx);
+
+        if (idx >= this.length || lIdx >= this.length)
+        {
+            return ;
+        }
+
+        const lV = this.data[lIdx];
+        const rV = this.data[rIdx];
+        const v = this.data[idx];
+
+        if (lV > rV && v < rV)
+        {
+            this.data[idx] = rV;
+            this.data[rIdx] = v;
+            this.heapifDown(rIdx);
+        }
+        else if (rV > lV && v > lV)
+        {
+            this.data[idx] = lV;
+            this.data[lIdx] = v;
+            this.heapifDown(lIdx);
+        }
+        
+
+        if (lIdx >= this.length)
+        {
+            return ;
+        }
+    }
+
+    private heapifyUp(idx : number) : void
+    {
+        if (idx === 0)
+        {
+            return ;
+        }
+        const p = this.parent(idx);
+        const parenV = this.data[p];
+        const v = this.data[idx];
+
+        if (parenV > v)
+        {
+            this.data[idx] = parenV;
+            this.data[p] = v;
+            this.heapifyUp(p);
+        }
+    }
+
+    private parent(idx : number): number
+    {
+        return Math.floor((idx - 1) / 2);
+    }
+
+    private leftChild(idx : number) : number
+    {
+        return (idx * 2 + 1);
+    }
+
+    private rightChild(idx : number) : number
+    {
+        return (idx * 2 + 2);
+    }
+}
